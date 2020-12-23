d8b1bd800a1c07d8f8c8d8ee55e78f9c13de0ae2
sarahoth <sarahanwar234@gmail.com>
2020-12-23 14:57:29 +0200

    add js file

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('hello in turkey');
+alert('hi agine' + prompt ('please enter your name')+'!welcome in my website about turkey');
+//var namecity = prompt('enter the name for best city in turkey for you :');
+//console.log(namecity);
+var time =prompt('enter the time  in Turkey');
+//document.write('<p>' + best_city + '</p>');
+var message;
+if(time < 12){
+    message = 'amm';
+}else{
+    message = 'pm';
+}
+alert(message);

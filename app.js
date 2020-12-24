b41114ab8456e83d277eab42ef80dede06fe5f21
sarahoth <sarahanwar234@gmail.com>
2020-12-24 15:50:17 +0200

    new updat

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-alert('hello in turkey');
-alert('hi agine' + prompt ('please enter your name')+'!welcome in my website about turkey');
+//alert('hello in turkey');
+//alert('hi agine' + prompt ('please enter your name')+'!welcome in my website about turkey');
 //var namecity = prompt('enter the name for best city in turkey for you :');
 //console.log(namecity);
-var time =prompt('enter the time  in Turkey');
+//var time =prompt('enter the time  in Turkey');
 //document.write('<p>' + best_city + '</p>');
-var message;
-if(time < 12){
-    message = 'amm';
-}else{
-    message = 'pm';
+//var message;
+//if(time < 12){
+  //  message = 'amm';
+//}else{
+  //  message = 'pm';
+
+//alert(message);
+
+var userName = prompt('Enter your name ');
+console.log(userName);
+
+while(userName === ''){
+    userName = prompt('enter your name');
+
 }
-alert(message);
+console.log(userName);
+
+
+var startN = prompt('enter the  start  number you want  to repit image');
+var endN = prompt('enter the end number you want to repit image');
+for(var i = startN;i<= endN; i++){
+    document.write(i + '<img src="Turkey.jpg">' );
+}
